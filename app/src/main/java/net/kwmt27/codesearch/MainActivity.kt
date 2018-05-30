51002ce0d49e2150a69b6397bb5e97b96e962d84
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-05-30 11:36:37 +0900

    Navigationライブラリを使ってNavigation Drawerが動くところまで

--- app/src/main/java/net/kwmt27/codesearch/MainActivity.kt
+++ app/src/main/java/net/kwmt27/codesearch/MainActivity.kt
@@ -1,19 +1,35 @@
 package net.kwmt27.codesearch
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import net.kwmt27.codesearch.ui.main.MainFragment
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import net.kwmt27.codesearch.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+        setSupportActionBar(binding.toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.main_nav_host_fragment)
+        setupActionBar(navController)
+        setupNavigationMenu(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean =
+            NavigationUI.navigateUp(binding.drawerLayout, Navigation.findNavController(this, R.id.main_nav_host_fragment))
+
+    private fun setupActionBar(navController: NavController) {
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+    }
+
+    private fun setupNavigationMenu(navController: NavController) {
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
 }
---
+++ app/src/main/java/net/kwmt27/codesearch/ui/repository/RepositoryListViewModel.kt
@@ -0,0 +1,7 @@
+package net.kwmt27.codesearch.ui.repository
+
+import android.arch.lifecycle.ViewModel;
+
+class RepositoryListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
